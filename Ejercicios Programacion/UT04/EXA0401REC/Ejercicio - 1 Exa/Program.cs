35da1e3c53d11b5d2032a3b1af0b0010f3852f8f
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-02-05 19:48:44 +0100

    Añadiendo temas

---
+++ Ejercicios Programacion/UT04/EXA0401REC/Ejercicio - 1 Exa/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            // Ejercicio - 3 | 
+            // Declaramos las variables
+            string CadenaInicial = "", valorReferencia = "", CadenaFinal = "";
+            bool Control = false;
+            int Contador = 0;
+
+            do
+            {
+                Console.WriteLine("Dime una cadenaInicial");
+                CadenaInicial = Console.ReadLine();
+
+                if (CadenaInicial.Length > 0)
+                {
+                    Console.WriteLine("Dime una cadena de referencia");
+                    valorReferencia = Console.ReadLine();
+
+                    if (valorReferencia.Length > 0)
+                    {
+                        Control = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR ! La cadena no puede estar vacia");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR ! La cadena no puede estar vacia");
+                }
+
+            } while (!Control);
+
+            for (int i = 0; i < CadenaInicial.Length; i++)
+            {
+                Contador = 0;
+                while (Contador < valorReferencia.Length)
+                {
+                    if (valorReferencia[Contador] == CadenaInicial[i])
+                    {
+                        Contador = valorReferencia.Length+1;
+                    }
+                    else
+                    {
+                        Contador++;
+                    }
+
+                    if (Contador == valorReferencia.Length)
+                    {
+                        Contador = valorReferencia.Length+1;
+                        CadenaFinal += CadenaInicial[i];
+                    }
+
+                }
+            }
+            
+            Console.WriteLine($"CadenaInical = [ {CadenaInicial} ]");
+            Console.WriteLine($"Cadena de Referencia = [ {valorReferencia} ]");
+            Console.WriteLine($"Cadena Final = [ {CadenaFinal} ]");
+            
+
+
+        }
+
+    }
+}
+
---
+++ Ejercicios Programacion/UT04/EXA0401REC/Ejercicio - 2 Exa/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            // Ejercicio 2 |
+            // Declaramos las variables
+            const int NUMEROMAXIMO = 8;
+            int[] VectorInicial = new int[NUMEROMAXIMO];
+            int[] VectorFinal;
+            int ContadorVeces = 1, NumeroUsuario = 0, valorReferencia = 0, ContadorPosicionVector = 0, tamañovectorfinal = 0, maximo = 0, ContadorVectorFinal = 0;
+            bool Control = false;
+
+            do
+            {
+                Console.WriteLine($"Dime el numero {ContadorVeces} del vector");
+                while (!int.TryParse(Console.ReadLine(), out NumeroUsuario))
+                {
+                    Console.WriteLine("ERROR ! El tipo de dato no es correcto");
+                }
+
+                if ((NumeroUsuario >= 100) && (NumeroUsuario <= 999))
+                {
+                    VectorInicial[ContadorPosicionVector] = NumeroUsuario;
+                    ContadorPosicionVector++;
+                    ContadorVeces++;
+
+                }
+                else
+                {
+                    Console.WriteLine("ERROR ! El número tiene que ser entero de tres cifras y positivos. Intentelo de nuevo");
+                }
+
+            }while(ContadorVeces <= NUMEROMAXIMO);
+
+            do
+            {
+                Console.WriteLine("Dime un número de referencia");
+                while (!int.TryParse(Console.ReadLine(), out valorReferencia))
+                {
+                    Console.WriteLine("ERROR ! El tipo de dato no es correcto");
+                }
+
+                if ((valorReferencia >= 100) && (valorReferencia <= 999))
+                {
+                    Control = true;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR ! El numero tiene que ser entero de 3 cifras y positivo");
+                }
+
+            } while (!Control);
+            Console.Clear();
+
+
+
+            Console.Write("VectorInical = {");
+            for (int i = 0; i < VectorInicial.Length; i++)
+            {
+                if (VectorInicial[i] < valorReferencia)
+                {
+                    tamañovectorfinal++;
+                }
+
+                Console.Write($" {VectorInicial[i]} , ");
+            }
+            Console.Write("}");
+
+            Console.WriteLine($"\nvalorReferencia = {valorReferencia}");
+
+            VectorFinal = new int[tamañovectorfinal];
+
+            for (int j = 0; j < VectorInicial.Length; j++)
+            {
+                if (VectorInicial[j] < valorReferencia)
+                {
+                    VectorFinal[ContadorVectorFinal] = VectorInicial[j];
+                    ContadorVectorFinal++;
+                }
+            }
+
+            maximo = VectorFinal[0];
+
+            Console.Write("VectorFinal = {");
+            for (int z = 0; z < VectorFinal.Length; z++)
+            {
+                if (maximo < VectorFinal[z])
+                {
+                    maximo = VectorFinal[z];
+                }
+               
+                Console.Write($" {VectorFinal[z]} , ");
+                
+            }
+            Console.Write("}");
+            Console.WriteLine($"\nmaximo = {maximo}");
+
+        }
+    }
+}
