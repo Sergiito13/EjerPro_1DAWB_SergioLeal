67e5a9983b24b6b93c04b1e492ae589e810efde3
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-02-09 13:17:35 +0100

    Modificacion ejercicio 2 y empezar ejercicio 3

---
+++ Ejercicios Programacion/UT05/EJE0504 - Ficheros/Ejercicio - 3/Funciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    public struct Alumnos
+    {
+        string[] NombreAlumno;
+        string[] PrimerApellido;
+        string[] SegundoApellido;
+        int[] NotaPrimeraEva;
+        int[] NotaSegundaEva;
+        int[] NotaTerceraEva;
+    }
+
+    class Funciones
+    {
+        public static Alumnos LeerDatosFichero()
+        {
+
+        }
+    }
+}
