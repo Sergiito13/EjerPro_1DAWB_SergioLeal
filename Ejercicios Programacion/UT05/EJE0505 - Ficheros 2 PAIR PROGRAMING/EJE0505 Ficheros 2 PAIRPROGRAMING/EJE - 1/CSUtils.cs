59905ce3e51605936470b0af771a4b739a12c86b
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-02-18 23:17:26 +0100

    Ejercicios de repaso ficheros

---
+++ Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 1/CSUtils.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionesEjercicioFich
+{
+    class CSFunciones
+    {
+        public const string RUTAFICHERO1 = "..\\..\\..\\1.txt";
+        public const string RUTAFICHERO2 = "..\\..\\..\\2.txt";
+        public const string RUTAFICHERODIFERENCIAS = "..\\..\\..\\Diferencias.txt";
+
+        public static void LeerFicherosyEscribirEnOtro()
+        {
+            // Declaramos las variables
+            string Linea1 = "", Linea2 = "";
+            int ContadorLinea = 0, ContadorLineaIguales = 0;
+            StreamReader SRead1 = null;
+            StreamReader SRead2 = null;
+            StreamWriter Swrite = null;
+           
+
+            try
+            {
+                SRead1 = new StreamReader(RUTAFICHERO1);
+                SRead2 = new StreamReader(RUTAFICHERO2);
+
+                
+
+                while ((!SRead1.EndOfStream) || (!SRead2.EndOfStream))
+                {
+                    Linea1 = SRead1.ReadLine();
+                    Linea2 = SRead2.ReadLine();
+                    ContadorLinea++;
+
+                    if (Linea1 != Linea2)
+                    {
+                        if (File.Exists(RUTAFICHERODIFERENCIAS))
+                        {
+                            Swrite.WriteLine($"{ContadorLinea};{Linea1};{Linea2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El fichero se ha creado correctamente");
+                            Swrite = File.CreateText(RUTAFICHERO2);
+                            Swrite.WriteLine($"{ContadorLinea};{Linea1};{Linea2}");
+                        }
+                        
+                    }
+                    else
+                    {
+                        ContadorLineaIguales++;
+
+                    }
+                }
+                Console.WriteLine($"El número de lineas que son iguales es de: {ContadorLineaIguales} lineas");
+                Swrite.Close();
+                SRead1.Close();
+                SRead2.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FuncionesEjercicioFich;
+
+namespace EJER1PairPrograming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Declaramos las variables
+
+            CSFunciones.LeerFicherosyEscribirEnOtro();
+
+        }
+    }
+}
