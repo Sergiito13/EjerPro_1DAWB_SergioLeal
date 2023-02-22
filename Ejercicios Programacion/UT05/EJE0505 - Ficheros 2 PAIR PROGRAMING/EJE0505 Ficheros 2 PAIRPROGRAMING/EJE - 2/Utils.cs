2ba5402d566810d3333181b3716672867d13087a
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-02-22 15:21:11 +0100

    EJER 2

---
+++ Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 2/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJE___2
+{
+    internal class Utils
+    {
+    }
+}
