adc4b3a526868575b164f9b53ddee3e01a4a64f8
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-02-23 16:15:03 +0100

    Ejer - 2 Avance

--- Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 2/Program.cs
+++ Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EJE2FICHEROS;
+using System;
+
+namespace EJER2FICHEROS
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            // Declaramos las variables
+            string NombreFich = "";
+            int NumeroLineas = 0;
+            
+            NombreFich = Funciones.PedirNombreFichero();
+
+            NumeroLineas = Funciones.PedirNumeroLineas();
+        }
+    }
+}
--- Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 2/Utils.cs
+++ Ejercicios Programacion/UT05/EJE0505 - Ficheros 2 PAIR PROGRAMING/EJE0505 Ficheros 2 PAIRPROGRAMING/EJE - 2/Utils.cs
@@ -4,9 +4,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EJE___2
+namespace EJE2FICHEROS
 {
-    internal class Utils
+    class Funciones
     {
+        public static string PedirNombreFichero() // ESTA FUNCION PEDIRA UN NOMBRE DE FICHERO
+        {
+            // Declramos las variables
+            const string DIGITOSNOVALIDOSFICHEROS = "/:*?<>|";
+            bool Salir = false, InvalidDigito;
+            string rutaFichero = "";
+
+            do
+            {
+
+                Console.WriteLine("Dime el nombre/ruta del fichero");
+                rutaFichero = Console.ReadLine();
+
+                if (rutaFichero.Length > 0)
+                {
+                    if (rutaFichero.Contains(".txt"))
+                    {
+                        InvalidDigito = false;
+                        foreach (char Digito in DIGITOSNOVALIDOSFICHEROS)
+                        {
+                            if (rutaFichero.Contains(Digito))
+                            {
+                                InvalidDigito = true;
+                            }
+                        }
+
+                        if (InvalidDigito)
+                        {
+                            Console.WriteLine("Error ! El nombre del fichero contiene algun caracter no valido");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El nombre ha sido aceptado por el programa");
+                            Salir = true;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error al nombre del fichero le falta la extension .txt");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error! el nombre del fichero no puede estar vacio");
+                }
+
+            } while (!Salir);
+            return rutaFichero;
+
+        }
+
+        public static int PedirNumeroLineas() // ESTA FUNCION PEDIRA X NUMERO DE LINEAS
+        {
+            // Declaramos las variables
+            int numeroLineas = 0;
+            bool Salir = false;
+
+            do
+            {
+                Console.WriteLine("Dime el número de lineas que quieres leer");
+                while (!int.TryParse(Console.ReadLine(), out numeroLineas))
+                {
+                    Console.WriteLine("Error el número tiene que ser de tipo entero");
+                }
+
+                if (numeroLineas < 0)
+                {
+                    Salir = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error el número no puede ser negativo");
+                }
+
+            } while (!Salir);
+            return numeroLineas;
+        } 
+
+        public static void AbrirFicheroLeerloMostrarLineas(String nombreFich, int numeroLineas) // ESTA FUNCION LEERA EL FICHERO
+        {
+            // Declaramos las variables
+            StreamReader SRead = null;
+
+            try
+            {
+                SRead = new StreamReader(nombreFich);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
