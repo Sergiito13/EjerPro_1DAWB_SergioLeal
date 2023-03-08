bc74f5510bedae141946f8fab707d6020431bd2d
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-03-08 19:44:45 +0100

    Ejercicio Tema 6 orientacion objetos

---
+++ Ejercicios Programacion/UT06/Ejercicio Practicas Objetos/Ejer - 1/Juego.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer1
+{
+    class Juego
+    {
+        // Atributos
+        private string nombre { get; set; }
+        private decimal precio { get; set; }
+        private string genero { get; set; }
+
+        // Constructores
+
+        public Juego(string nombre,decimal precio, string genero)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.genero = genero;
+        }
+
+        // Metodos
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public decimal GetPrecio()
+        {
+            return this.precio;
+        }
+        public string GetGenero()
+        {
+            return this.genero;
+        }
+
+
+        public static List<Juego> AñadirNuevoJuego(List<Juego> juegos)
+        {
+            // Declaramos las variables
+            
+            bool salir = false;
+            string nombreJuego = "", generoJuego = "";
+            decimal precioJuego = 0;
+
+            do
+            {
+                Console.WriteLine("Dime un nombre de un juego");
+                nombreJuego = Console.ReadLine();
+
+                if (nombreJuego.Length > 0)
+                {
+                    salir = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error! La cadena no puede estar vacia");
+                }
+            } while (!salir);
+
+            salir = false;
+
+            do
+            {
+                Console.WriteLine("Dime un precio para el juego nuevo");
+                while (!decimal.TryParse(Console.ReadLine(), out precioJuego))
+                {
+                    Console.WriteLine("Error ! Tiene que ser de tipo decimal el dato");
+                }
+                if (precioJuego > 0)
+                {
+                    salir = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error ! El precio no puede ser negativo");
+                }
+
+            } while (!salir);
+
+            salir = false;
+
+            do
+            {
+                Console.WriteLine("Dime un genero para el juego");
+                generoJuego = Console.ReadLine();
+
+                if (generoJuego.Length > 0)
+                {
+                    salir = true;
+                    juegos.Add(new Juego(nombreJuego, precioJuego, generoJuego));
+                }
+                else
+                {
+                    Console.WriteLine("Error ! La cadena no puede estar vacia");
+                }
+
+            } while (!salir);
+            return juegos;
+        }
+
+        
+        public static void MostrarLosJuegos(List<Juego> juegos)
+        {
+            // Declaramos las variables
+            Console.WriteLine("Lista de juegos: ");
+
+            foreach (Juego juego in juegos)
+            {
+                Console.WriteLine(juego.GetNombre());
+                Console.WriteLine(juego.GetPrecio());
+                Console.WriteLine(juego.GetGenero());
+                Console.WriteLine("---------------------------");
+
+            }
+            Console.WriteLine("Pulsa una tecla para continuar.");
+            Console.ReadKey();
+
+               
+        }
+    }
+}
---
+++ Ejercicios Programacion/UT06/Ejercicio Practicas Objetos/Ejer - 1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejer1
+{
+    class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            // Declaramos las variables
+            List<Juego> juegos = new List<Juego>();
+            int opcion = 0;
+            bool salir = false;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("-----------------Menu--------------------");
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("- 1.- Añadir Juego                      -");
+                Console.WriteLine("- 2.- Mostrar Lista de juegos           -");
+                Console.WriteLine("- 3.- Salir                             -");
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("");
+
+                Console.WriteLine("Dime una opcion del menu");
+                while (!int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    Console.WriteLine("ERROR !");
+                }
+                if ((opcion >= 1)||(opcion <= 2))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            {
+                                juegos = Juego.AñadirNuevoJuego(juegos);
+                            }
+                            ; break;
+                        case 2:
+                            {
+                                Juego.MostrarLosJuegos(juegos);
+                            }
+                            ; break;
+                        case 3: salir = true; 
+                            break;    
+                    }
+                }  
+            } while (!salir);
+            
+
+        }
+    }
+}
