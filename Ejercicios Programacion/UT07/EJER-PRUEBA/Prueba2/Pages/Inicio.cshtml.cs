2bb2dadef3230ebbe64104b508647a03047782e9
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-05-18 13:26:55 +0200

    Ejer

---
+++ Ejercicios Programacion/UT07/EJER-PRUEBA/Prueba2/Pages/Inicio.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EJER1_PRUEBA.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class InicioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
