f79880e75992ca02f0ecca684c32fc129f15f5d6
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-05-20 19:28:12 +0100

    Avance ejer0701

---
+++ Ejercicios Programacion/UT07/EJE0701–Razor/EJER/Pages/resultadoEjer2.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EJER.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class resultadoEjer2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
