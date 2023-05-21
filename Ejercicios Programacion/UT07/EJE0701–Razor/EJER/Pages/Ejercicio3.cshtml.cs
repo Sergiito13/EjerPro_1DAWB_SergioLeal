e978b7b8960999978a3e96ed34dbd6bb1435c08c
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-05-21 02:08:46 +0100

    EJER0701 ACABADO

--- Ejercicios Programacion/UT07/EJE0701–Razor/EJER/Pages/Ejercicio3.cshtml.cs
+++ Ejercicios Programacion/UT07/EJE0701–Razor/EJER/Pages/Ejercicio3.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EJER.Pages
 {
+    [IgnoreAntiforgeryToken(Order = 1001)]
     public class Ejercicio3Model : PageModel
     {
         public void OnGet()
