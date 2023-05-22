80303a32d5516964351a69b12013888dc90b6858
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-05-22 21:30:57 +0200

    Ejer4 razor

---
+++ Ejercicios Programacion/UT07/EJE0701–Razor/EJER/Pages/Ejercicio4.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EJER.Pages
+{
+    public class Ejercicio4Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
